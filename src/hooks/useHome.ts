536d247086202575d44b6310db18162fd891c8f4
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2025-08-21 17:46:41 -0700

    fixed sorting

--- src/hooks/useHome.ts
+++ src/hooks/useHome.ts
@@ -10,12 +10,18 @@ export function useHome() {
 
     const allStories = messages.map(str=>JSON.parse(str));
 
+    function strCompare(str1: string, str2: string) {
+        if (str1 < str2) return -1;
+        if (str1 > str2) return 1;
+        return 0;
+    }
+
     function getTop() {
         return getNewest();
     }
 
     function getNewest() {
-        return allStories.sort(story=>story.createdAt).slice(0,10);
+        return allStories.sort((a,b)=>strCompare(b.createdAt, a.createdAt)).slice(0,10);
     }
 
     function getLatestUpdate() {
