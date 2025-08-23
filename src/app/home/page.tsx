5605b42d1851919712aeed3b677a383dbf5f0367
TheTastyPi <48721067+TheTastyPi@users.noreply.github.com>
2025-08-22 18:20:44 -0700

    BlogCard initial implementation

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Announcement } from "@/components/shared/Announcement";
+import BlogCard from "@/components/shared/BlogCard";
 import Navbar from "@/components/shared/NavigationBar";
 import { StoryList } from "@/components/shared/StoryList";
 import { useHome } from "@/hooks/useHome";
@@ -13,6 +14,10 @@ export default function HomePage() {
       <Navbar />
 
       <main className="flex-1 p-8 sm:p-20 flex flex-col gap-8">
+        <BlogCard id={"1234"} title={"Test Title"} createdAt={"Aug 22, 2025, 3:27 PM"} views={0} author={{
+          displayName: "TheTastyPi",
+          firebaseUid: "3414134314"
+        }} content={"This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee This is a blog. Yippee "} fetchData={false} />
         <Announcement stories={getTop()}/>
         <div className="flex gap-4 sm:flex-row flex-col">
           <StoryList title="New Stories" stories={getNewest()} />
---
+++ src/components/shared/BlogCard.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { BlogCardProp } from "@/types/blog";
+
+export default function BlogCard(props: BlogCardProp & { fetchData?: boolean }) {
+  const [blogCardData, setBlogPostData] = useState<BlogCardProp | null>(null);
+
+  useEffect(() => {
+    async function fetchBlogPostDetails() {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/blogs/${props.id}`
+        );
+        if (!res.ok) throw new Error("Failed to fetch blog");
+        const data = await res.json();
+
+        setBlogPostData({
+          id: data.id,
+          author: {
+            displayName: data.author.name,
+            firebaseUid: data.author.firebaseUid
+          },
+          createdAt: new Date(data.createdAt).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric"
+          }),
+          title: data.title,
+          content: data.content,
+          views: data.views
+        });
+      } catch (err) {
+        console.error("Error fetching story details:", err);
+      }
+    }
+
+    if (props.fetchData || props.fetchData === undefined) fetchBlogPostDetails();
+  }, [props.fetchData, props.id]);
+
+  const shortened = (blogCardData?.content || props.content).length > 400;
+  const shownContent = (blogCardData?.content || props.content).slice(0,400) + (shortened ? "..." : "");
+  const blogURL = `/blog/${blogCardData?.id || props.id}`;
+
+  return (
+    <Card className={`w-full gap-1`}>
+      <CardHeader>
+        <CardTitle className="flex flex-col sm:flex-row sm:items-center sm:gap-2">
+          <a
+            href={blogURL}
+            className="text-ellipsis font-bold hover:underline text-xl"
+          >
+            {blogCardData?.title || props.title}
+          </a>
+          <div className="text-gray-500">
+            {blogCardData?.createdAt || props.createdAt}
+          </div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col justify-evenly gap-4 pb-4">
+        {shownContent}
+        {shortened ? <a className="text-blue-600 hover:underline" href="blogURL">Read More</a> : <></>}
+      </CardContent>
+
+      <CardFooter>
+        <div className="rounded-sm bg-gray-100 w-full p-2 text-sm text-gray-800 flex gap-1 justify-between">
+          <div className="flex items-center gap-2">
+            <Link
+              href={"/user/" + props.author.firebaseUid}
+              className="hover:underline"
+            >
+              {blogCardData?.author.displayName || props.author.displayName || "..."}
+            </Link> • {blogCardData?.views || props.views} views
+          </div>
+          <a 
+            href={`/report`} // placeholder
+            className="hover:bg-red-400 rounded-sm p-1 font-bold"
+          >Report</a>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
